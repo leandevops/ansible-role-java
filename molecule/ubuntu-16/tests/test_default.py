95ea33ae1c68d0fbe39ee49ea96db0cbbb116992
Andrey Larin <lestex@gmail.com>
2019-03-23 11:31:13 +0300

    updating the role

--- molecule/ubuntu-16/tests/test_default.py
+++ molecule/ubuntu-16/tests/test_default.py
@@ -14,3 +14,20 @@ def test_java_binary(host, command):
     cmd = host.run('. /etc/profile && ' + command + ' -version')
     assert cmd.rc == 0
     assert '1.8.0_' in cmd.stderr
+
+
+@pytest.mark.parametrize('version_dir_pattern', [
+    '1\\.8\\.0_[0-9]+$'
+])
+def test_java_installed(host, version_dir_pattern):
+
+    java_home = host.check_output(
+                        'find `$JAVA_HOME` | grep --color=never -E %s',
+                        version_dir_pattern)
+
+    java_bin = host.file(java_home + '/bin/java')
+
+    assert java_bin.exists
+    assert java_bin.is_file
+    assert java_bin.user == 'root'
+    assert java_bin.group == 'root'
